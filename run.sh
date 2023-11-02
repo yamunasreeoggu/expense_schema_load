b7e77923bd7f9e3cdd234c4250fe5f3380258ec1
yamunasreeoggu <yamunasree321@gmail.com>
2023-11-02 17:25:30 -0400

    creating eks cluster

--- run.sh
+++ run.sh
@@ -1,4 +1,6 @@
-sleep 30 # this is needed because schema should load only after mysql pod is available
+source /config/params
+
+# sleep 30 # this is needed because schema should load only after mysql pod is available
 git clone https://github.com/yamunasreeoggu/${COMPONENT}
 cd ${COMPONENT}/schema
 mysql -h${DBHOST} -u${DBUSER} -p${DBPASS} <${COMPONENT}.sql
